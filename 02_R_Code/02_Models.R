d675b6e2d875ed1b6da0e25aa384be2ddb0e1a64
Gen-Chang Hsu <genchanghsu@gmail.com>
2023-12-15 22:20:46 -0500

    Update 02_Models.R

--- 02_R_Code/02_Models.R
+++ 02_R_Code/02_Models.R
@@ -26,6 +26,30 @@ carcass_data_clean <- read_csv("./03_Outputs/Data_Clean/Carcass_Data_Clean.csv")
 
 # 1. Data exploration and summary ----------------------------------------------
 
+### (1) Taxon compositions of the wild carcasses
+carcass_data_clean %>% 
+  filter(carcass_type == "wild") %>% 
+  group_by(carcass_taxon) %>% 
+  summarise(n = n()) %>% 
+  mutate(prop = n/sum(n))
+
+### (2) Weight of the wild and lab carcasses
+carcass_data_clean %>% 
+  filter(carcass_type == "wild") %>% 
+  pull(carcass_weight) %>% 
+  summary()
+
+carcass_data_clean %>% 
+  filter(carcass_type == "lab") %>% 
+  pull(carcass_weight) %>% 
+  summary()
+
+ggplot(carcass_data_clean) + 
+  geom_histogram(aes(x = carcass_weight, fill = carcass_type)) + 
+  facet_wrap(~ carcass_type, nrow = 2) + 
+  scale_fill_brewer(palette = "Set1")
+
+### (3) Parent size
 
 
 
